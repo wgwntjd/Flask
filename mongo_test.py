1b1f345a658fb17b5d5f450a58da48d37dc99a00
wgwntjd <k22675427@gmail.com>
2020-05-31 00:02:39 +0900

    2nd commit

--- mongo_test.py
+++ mongo_test.py
@@ -1,11 +1,16 @@
 from pymongo import MongoClient
 from flask import Flask, request, redirect, url_for, render_template
+app = Flask(__name__)
 
+@app.route('/')
 @app.route('/mongo', methods=['POST'])
 def mongoTest():
     client = MongoClient('mongodb://localhost:27017/')
-    db = client.newDatabase
-    collection = db.mongoTest
+    db = client.sample
+    collection = db.user
     results = collection.find()
     client.close()
     return render_template('mongo.html', data=results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
